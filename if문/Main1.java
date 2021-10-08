a0c03a4d971a79e9209e79dc053fb1ba1f0c0f59
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:23:28 +0900

    Create Main1.java

---
+++ if문/Main1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main1{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+        if(a>b)
+            System.out.print(">");
+        else if(a<b)
+            System.out.print("<");
+        else if(a==b)
+            System.out.print("==");
+        
+        scan.close();
+    }
+}
