142faa692bc6c2d6e5cb2a17b6834025bcf43969
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:36:14 +0900

    Create Main5.java

---
+++ 1차원 배열/Main5.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main5 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		double score[] = new double[num];
+		
+		for(int i=0; i<score.length; i++) {
+			score[i] = scan.nextInt();
+		}
+		
+		scan.close();
+		
+		Arrays.sort(score);
+		
+		double sum = 0;
+		
+		for(int i=0; i<score.length; i++) {
+			sum += (score[i]/score[score.length-1])*100;
+		}
+		
+		System.out.print(sum/score.length);
+	}
+}
