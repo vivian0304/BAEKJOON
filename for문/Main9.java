597e4e0dd4f6813c4e8feabe403aafff72875638
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:31:28 +0900

    Create Main9.java

---
+++ for문/Main9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main9{
+    public static void main(String args[]){
+    	
+        Scanner scan = new Scanner(System.in);
+        
+        int num = scan.nextInt();
+        
+        for(int i=0; i<num; i++){
+            for(int j=0; j<=i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+            
+    }
+}
