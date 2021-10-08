853673decbca84d776958b78299cc9afaf3af739
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:24:36 +0900

    Create Main4.java

---
+++ if문/Main4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main4{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+        if(a > 0 && b > 0)
+            System.out.print(1);
+        else if(a < 0 && b > 0)
+            System.out.print(2);
+        else if(a < 0 && b < 0)
+            System.out.print(3);
+        else if(a > 0 && b < 0)
+            System.out.print(4);
+        
+        scan.close();
+    }
+}
