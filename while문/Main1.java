63c6fb12e2fdf2e7af51314a7d41c0083504fd83
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:32:44 +0900

    Create Main1.java

---
+++ while문/Main1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main1{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        List<Integer> sumArray = new ArrayList<Integer>();
+        int a, b;
+        
+        do{
+            a = scan.nextInt();
+            b = scan.nextInt();
+            
+            sumArray.add(a+b);
+        } while(a != 0 && b != 0);
+        
+        for(int i=0; i<sumArray.size()-1; i++)
+            System.out.println(sumArray.get(i));
+        
+        scan.close();
+    }
+}
