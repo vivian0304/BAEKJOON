26d92d88daba79e45af1be10800c1db198c6498b
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:38:42 +0900

    Create Main1.java

---
+++ 문자열/Main1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main1 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		char a = scan.next().charAt(0);
+		
+		scan.close();
+		
+		System.out.println((int)a);
+	}
+}
