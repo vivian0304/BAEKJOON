9265b53e36345ed9189fd931c5e7ae547e6a7af2
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:28:41 +0900

    Create Main2.java

---
+++ for문/Main2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main2{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int a[] = new int[num];
+        int b[] = new int[num];
+        int sum[] = new int[num];
+        
+        for(int i=0; i<num; i++){
+            a[i] = scan.nextInt();
+            b[i] = scan.nextInt();
+            sum[i] = a[i]+b[i];
+        }
+        
+        for(int i=0; i<num; i++){
+            System.out.println(sum[i]);
+        }
+        
+        scan.close();
+    }
+}
