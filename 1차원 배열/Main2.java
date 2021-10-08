889c271bf43faaeaff0057dad4757c2d45901738
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:34:46 +0900

    Create Main2.java

---
+++ 1차원 배열/Main2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main2{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        
+        int ary[] = new int[9];
+        
+        for(int i=0; i<9; i++)
+            ary[i] = scan.nextInt();
+        
+        int origin[] = ary.clone();
+        
+        Arrays.sort(origin);
+        System.out.println(origin[origin.length-1]);
+        
+        for(int i=0; i<9; i++) 
+    		if(origin[origin.length-1] == ary[i])
+    			System.out.print(i+1);
+            
+        scan.close();
+    }
+}
