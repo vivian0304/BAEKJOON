a20c73bb2da7b661b33225f9d807ed20d4abc451
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:30:21 +0900

    Create Main6.java

---
+++ for문/Main6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main6{
+    public static void main(String args[]){
+    	Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        
+        for(int i = a; i>=1; i--)
+            System.out.println(i);
+    }
+}
