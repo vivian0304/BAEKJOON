0acd098b6f9da2a3c92c03fb380be15f21c8eff7
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:29:04 +0900

    Create Main3.java

---
+++ for문/Main3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main3{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+        
+        for(int i=1; i<=num; i++){
+            sum += i;
+        }
+        
+        System.out.println(sum);
+        
+        scan.close();
+    }
+}
