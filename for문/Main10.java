13dcb735f6068756674051178bb85195ab9769b9
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:31:51 +0900

    Create Main10.java

---
+++ for문/Main10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main10{
+    public static void main(String args[]){
+    	
+        Scanner scan = new Scanner(System.in);
+        
+        int num = scan.nextInt();
+        
+        for(int i=0; i<num; i++){
+            for(int j=0; j<num-i-1; j++)
+                System.out.print(" ");
+            for(int j=0; j<=i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+            
+    }
+}
