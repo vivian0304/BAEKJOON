8917d7534326cb9813c44834c8315eb26b181218
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:38:59 +0900

    Create Main2.java

---
+++ 문자열/Main2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main2 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		char charArr[] = new char[num];
+		
+		charArr = scan.next().toCharArray();
+		
+		int sum = 0;
+		for(int i=0; i<charArr.length; i++) {
+			sum += charArr[i] - '0';
+		}
+		
+		System.out.print(sum);
+		
+		scan.close();
+	}
+}
