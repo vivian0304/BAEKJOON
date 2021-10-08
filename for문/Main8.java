f4783033159b13dc7923463089294817715aab50
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:31:06 +0900

    Create Main8.java

---
+++ for문/Main8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main8{
+    public static void main(String args[]){
+    	
+        Scanner scan = new Scanner(System.in);
+        
+        int num = scan.nextInt();
+        int a[] = new int[num];
+        int b[] = new int[num];
+        int sum[] = new int[num];
+
+        for(int i=0; i<num; i++){
+            a[i] = scan.nextInt();
+            b[i] = scan.nextInt();
+
+            sum[i] = a[i] + b[i];
+        }
+        for(int i=0; i<num; i++){
+            System.out.println("Case #"+(i+1)+": "+a[i]+" + "+b[i]+" = "+sum[i]);
+        }
+
+        
+    }
+}
