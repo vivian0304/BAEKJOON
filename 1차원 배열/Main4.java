e0c0399ef1e41d4f1c83051cbf632e848fb05bec
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:35:46 +0900

    Create Main4.java

---
+++ 1차원 배열/Main4.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main4 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num[] = new int[10];
+		int div[] = new int[10];
+		
+		for(int i=0; i<num.length; i++) {
+			num[i] = scan.nextInt();
+		}
+		
+		for(int i=0; i<div.length; i++) {
+			div[i] = num[i]%42;
+		}
+		
+		int sum = 1;
+		Arrays.sort(div);
+		
+		for(int i=1; i<div.length; i++) {
+			if(div[i-1] != div[i])
+				sum++;
+		}
+		
+		System.out.print(sum);
+		
+		scan.close();
+	}
+}
