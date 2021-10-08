49a95c1d4a35a9cb8a20e39b40a2d30c1cd03702
vivian0304 <59956020+vivian0304@users.noreply.github.com>
2021-10-09 06:41:04 +0900

    Create Main6.java

---
+++ 문자열/Main6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main6 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine().trim();
+		
+		scan.close();
+		
+		if(a.isEmpty()) {
+			System.out.println(0);
+		} else {
+			String num[] = a.split(" ");
+			System.out.println(num.length);
+		}
+		
+		
+	}
+}
